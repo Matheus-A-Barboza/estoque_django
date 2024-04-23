cb2996cd5b48e3159a6066064bd09eda49df3fbf
Matheus <progammerantunes@gmail.com>
2024-04-23 20:04:47 -0300

    Commit

--- estoqueADS/views.py
+++ estoqueADS/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render, HttpResponse
 from .models import Produtos
+from datetime import datetime
 
 def index(request):
     produtos = Produtos.objects.all()
     return render(request, 'pages/index.html', {'produtos': produtos})
+
+def adicionar_produto(request):
+    
+    if request.method == "POST":
+        nome = request.POST['nome']
+        preco = request.POST['preco']
+        descricao = request.POST['descricao']
+        quantidade = request.POST['quantidade']
+        codigo = request.POST['codigo']
+        em_estoque = False
+        
+        if quantidade > 0:
+            em_estoque = True
+        data_criacao = datetime.now()
+        
+        
+    else:
+        return render(request, 'pages/adicionar_produto.html')
+    
+    return render(request, 'pages/adicionar_produto.html')
