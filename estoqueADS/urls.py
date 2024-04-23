1da77002a47bd304deb4e0d4425628fccd85953c
Matheus <progammerantunes@gmail.com>
2024-04-23 20:04:47 -0300

    Commit

--- estoqueADS/urls.py
+++ estoqueADS/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    # path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('adicionar_produto/', views.adicionar_produto, name='adicionar_produto')
 ]
